8c3f5fed2043fc0705ce5fb43e6214b7283fa7d4
Yyyye <a88898552@live.com>
2017-12-14 22:33:55 +0800

    create first cpp file
    myfirst

---
+++ Pratice/Pratice/myfirst.cpp
@@ -0,0 +1,10 @@
+#include<iostream>  //预处理编译指令
+int main()//函数头
+{  // {} 函数体
+	using namespace std; //编译指令
+	cout << "Come up and C++ me some time.";   // 使用cout的显示消息语句
+	cout << endl;
+	cout << "You wont't regret it !" << endl;
+	cin.get();  //停留   
+	return 0;  // 结束main的return语句
+}
