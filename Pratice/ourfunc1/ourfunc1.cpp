2114eac524f07e7f7a7e578bc4a7359ca77c3b52
Yyyye <a88898552@live.com>
2017-12-15 03:18:01 +0800

    测试了下  可以在函数原型中添加变量名

--- Pratice/ourfunc1/ourfunc1.cpp
+++ Pratice/ourfunc1/ourfunc1.cpp
@@ -4,7 +4,7 @@ using namespace std;  //		全局使用
 // 1.将using 放在函数里 让函数可以访问std的全部内容
 // 2.特定函数中  也可以使用 using  std::cout;(使用指定的元素 cout)
 // 3.完全不使用using  直接 使用 std:: 可以访问某个特定的元素
-void  simon(int);
+void  simon(int n);
 int main()
 { 
 	simon(3);
@@ -13,6 +13,8 @@ int main()
 	cin >> count;
 	simon(count);
 	cout << "Done! " << endl;
+	cin.get();
+	cin.get();
 	return 0;
 }
 
